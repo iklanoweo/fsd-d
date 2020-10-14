bc7674a59a51cd9f54a7298e153bf8b207cdfcad
iklanoweo <72304816+iklanoweo@users.noreply.github.com>
2020-10-14 20:22:07 +0700

    Add files via upload

---
+++ Probabilitas.R
@@ -0,0 +1,11 @@
+###Binomial Random Variable###
+
+
+###
+### Misalnya, Indra melempar kelereng ke lubang sebanyak 10 kali
+### X adalah frek Indra berhasil memasukkan sebanyak 2 kali
+### dan probabilitas keberhasilannya adalah 1/5 dari peluang sebanyak 10 kali
+### Berapa probabilitasnya
+### Bahwa Indra memasukkan x = 2, dan sukses dari 10 kali percobaan?
+
+dbinom(x = 2, size = 10, prob = 1/5)
